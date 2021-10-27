a482cc07df0ed069b1c95c164417409876f92bd7
Simon Riple <simon.riple@knowit.no>
2021-10-27 15:24:35 +0200

    mainframe with local data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
+import { useState } from "react";
 import trips from "./data/trips";
-import Trip from "./Trip";
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
       <Header />
-        {trips.map((trip) => (
-          <Trip
-            key={trip.id}
-            username={trip.username}
-            caption={trip.caption}
-            position={trip.position}
-            image={trip.image}
-          />
-        ))}
+      {(function renderTrips() {
+        const tripcomponents = [];
+        for (let i = 0; i < trips.length; i++) {
+          const trip = trips[i];
+          tripcomponents.push(
+            <Trip
+              key={trip.id}
+              username={trip.username}
+              caption={trip.caption}
+              position={trip.position}
+            >
+              <Image src={trip.image} alt={trip.caption}/>
+            </Trip>
+          );
+        }
+        return tripcomponents;
+      })()}
     </div>
   );
 }
 
-const Header = () => {
+function Header() {
   return (
     <div className="site-header">
       <div className="title">
@@ -27,5 +35,49 @@ const Header = () => {
     </div>
   );
 };
+
+function Trip(props) {
+  function makeEarthLink(position) {
+    return (
+      "https://www.google.com/maps/place/" +
+      position.latitude +
+      "," +
+      position.longitude
+    );
+  }
+
+  return (
+    <div className="trip">
+      <div className="header">{props.username}</div>
+      {props.children}
+      <div className="footer">
+        <p>{props.caption}</p>
+        <a className="earth" href={makeEarthLink(props.position)}>
+          🌍
+        </a>
+        <Likes />
+      </div>
+    </div>
+  );
+};
+
+function Image(props) {
+  return <img src={props.src} alt={props.alt} className="image" />;
+};
+
+function Likes() {
+  const [likes, setLikes] = useState(0);
+  function addLike() {
+    setLikes(likes + 1);
+  }
+  return (
+    <div className="likes">
+      Likes: {likes}
+      <button className="like-button" onClick={addLike}>
+        👍
+      </button>
+    </div>
+  );
+}
 
 export default App;
